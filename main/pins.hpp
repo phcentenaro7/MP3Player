aee9c93897016a5c73491bd0f4e5a13314214ac5
Pedro Centenaro <phc7blu@gmail.com>
2024-10-30 08:28:42 -0300

    Corrected typo in pin SCLK

--- main/pins.hpp
+++ main/pins.hpp
@@ -6,7 +6,7 @@ namespace PlayerPins
     const uint8_t SPI_CS = 5;
     const uint8_t SPI_MOSI = 23;
     const uint8_t SPI_MISO = 19;
-    const uint8_t SPI_SLCK = 18;
+    const uint8_t SPI_SCLK = 18;
 }
 
 #endif
