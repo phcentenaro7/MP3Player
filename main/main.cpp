0e7642dff491aca51ee870cbb3efdb544c49b0d0
Pedro Centenaro <phc7blu@gmail.com>
2024-11-10 11:11:45 -0300

    Modified to test latest features

--- main/main.cpp
+++ main/main.cpp
@@ -19,26 +19,8 @@ extern "C" void app_main()
 	while (!Serial) { }
 
 	PlayerSD::Initialize();
-	File root = SD.open("/");
-	PlayerSD::ShowFileTree(root);
-	File file;
-	while(true)
-	{
-		file = root.openNextFile();
-		if(!file.isDirectory())
-		{
-			break;
-		}
-		file.close();
-	}
-	if(PlayerID3::FileHasID3Tag(file))
-	{
-		printf("File %s has ID3 tag\n", file.name());
-		PlayerID3::ID3Header header = PlayerID3::ID3Header(file);
-		printf("major version: %d\n", header.getMajorVersion());
-		printf("revision number: %d\n", header.getRevisionNumber());
-		printf("flags: %d\n", header.getFlags());
-		printf("size: %ld\n", header.getTagSize());
-	}
-	file.close();
+	File f = SD.open("/001.MP3");
+	PlayerID3::ID3Tag tag = PlayerID3::ID3Tag(f);
+	tag.Print();
+	f.close();
 }
