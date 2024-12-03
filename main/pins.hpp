8de97c931a77ed3a649b54b4c00edf5d0b350004
Pedro Centenaro <phc7blu@gmail.com>
2024-12-03 19:35:18 -0300

    Added SCL and SDA pins for I2C communication

--- main/pins.hpp
+++ main/pins.hpp
@@ -7,6 +7,8 @@ namespace PlayerPins
     const uint8_t SPI_MOSI = 23;
     const uint8_t SPI_MISO = 19;
     const uint8_t SPI_SCLK = 18;
+    const uint8_t I2C_SCL = 13;
+    const uint8_t I2C_SDA = 12;
 }
 
 #endif
