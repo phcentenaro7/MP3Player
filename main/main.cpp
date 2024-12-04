bc31faf1cb27f59490a03bf06f49e2a289e32d69
Pedro Centenaro <phc7blu@gmail.com>
2024-12-04 12:07:47 -0300

    Changed to test custom character and display on/off

--- main/main.cpp
+++ main/main.cpp
@@ -21,11 +21,23 @@ extern "C" void app_main()
 
 	LCD_init(0x27, PlayerPins::I2C_SDA, PlayerPins::I2C_SCL, 16, 2);
 	LCD_clearScreen();
+	char folderUp[8] = 
+	{
+		0b00100,
+		0b01010,
+		0b01010,
+		0b10001,
+		0b10001,
+		0b00000,
+		0b00000,
+		0b00000
+	};
+	LCD_writeToCGRAM(0, folderUp);
 	while(true)
 	{
 		LCD_setDisplayOn();
 		LCD_setCursor(0, 0);
-		LCD_writeStr("Que coisa boa");
+		LCD_writeChar(0);
 		vTaskDelay(2000 / portTICK_PERIOD_MS);
 		LCD_setDisplayOff();
 		vTaskDelay(2000 / portTICK_PERIOD_MS);
