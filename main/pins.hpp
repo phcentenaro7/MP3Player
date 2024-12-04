be97480f0c30622c63c01f282bbbb82da6b5cc9d
Pedro Centenaro <phc7blu@gmail.com>
2024-12-04 19:29:06 -0300

    Added button pins

--- main/pins.hpp
+++ main/pins.hpp
@@ -9,6 +9,9 @@ namespace PlayerPins
     const uint8_t SPI_SCLK = 18;
     const uint8_t I2C_SCL = 33;
     const uint8_t I2C_SDA = 32;
+    const uint8_t BUTTON_CHOICE_1 = 14;
+    const uint8_t BUTTON_CHOICE_2 = 12;
+    const uint8_t BUTTON_SWITCH = 13;
 }
 
 #endif
