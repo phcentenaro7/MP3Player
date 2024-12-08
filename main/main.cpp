e83a6805ef6ef48c2d7823bb6e3dead5a8f50bc7
Pedro Centenaro <phc7blu@gmail.com>
2024-12-07 23:04:09 -0300

    Changed the code to make it work with the new System class

--- main/main.cpp
+++ main/main.cpp
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <string.h>
 #include "freertos/FreeRTOS.h"
+#include "system.hpp"
 #include "sd_card.hpp"
 #include "id3.hpp"
 #include "HD44780.h"
@@ -16,30 +17,23 @@
 
 static const char* TAG = "main";
 static bool readerDone = false;
-static PlayerSD::FileSystemManager* fileSystem;
-static PlayerLCD::LCD* lcd;
-    
+static PlayerSD::FileSystemManager* fileSystem = NULL;
+static PlayerLCD::LCD* lcd = NULL;
+static PlayerSystem::System* sys = NULL;
+
 void AwaitCardInReader();
 void RetrieveCardData();
 void AwaitCardInPlayer();
 
 extern "C" void app_main()
 {
+	esp_log_level_set("*", ESP_LOG_DEBUG);
 	ESP_LOGI(TAG, "Starting MP3Player");
 	initArduino();
   	Serial.begin(115200);
 	while(!Serial){};
-	while(!DF_begin(PlayerPins::SERIAL_TX, PlayerPins::SERIAL_RX, true, true, true))
-	{
-		puts("Trying again");
-		vTaskDelay(1000 / portTICK_PERIOD_MS);
-	};
 
 	lcd = new PlayerLCD::LCD();
-
-	pinMode(PlayerPins::BUTTON_CHOICE_1, INPUT_PULLDOWN);
-	pinMode(PlayerPins::BUTTON_CHOICE_2, INPUT_PULLDOWN);
-	pinMode(PlayerPins::BUTTON_SWITCH, INPUT_PULLDOWN);
 
 	while(true)
 	{
@@ -59,33 +53,39 @@ extern "C" void app_main()
 
 void AwaitCardInReader()
 {
-	lcd->Write("Insert SD card", "in reader!");
+	lcd->Write("Insert SD card", 0);
+	lcd->Write("in reader!", 1);
 	while(!PlayerSD::Initialize()){};
 }
 
 void RetrieveCardData()
 {
-	lcd->Write("SD inserted!", "");
+	lcd->Write("SD inserted!", 0);
+	lcd->Write("", 1);
 	vTaskDelay(3000 / portTICK_PERIOD_MS);
-	lcd->Write("Loading...", NULL);
+	lcd->Write("Loading...", 0);
 	fileSystem = new PlayerSD::FileSystemManager(lcd);
+	//sys = new PlayerSystem::System(fileSystem);
 	readerDone = true;
 }
 
 void AwaitCardInPlayer()
 {
-	lcd->Write("Ready to play!", "");
+	lcd->Write("Ready to play!", 0);
+	lcd->Write("", 1);
 	vTaskDelay(10000 / portTICK_PERIOD_MS);
-	lcd->Write("Playing!", NULL);
-	DF_volume(30);
-	playNext:
-	vTaskDelay(1000);
-	DF_next();
-	vTaskDelay(1000);
-	int finished = 0;
-	while(!DF_isFinished(&finished))
+	sys = new PlayerSystem::System(fileSystem, lcd);
+	lcd->Write("Playing!", 0);
+	while(true)
 	{
-		vTaskDelay(10 / portTICK_PERIOD_MS);
+		vTaskDelay(1000 / portTICK_PERIOD_MS);
 	}
-	goto playNext;
+	// DF_volume(30);
+	// vTaskDelay(100 / portTICK_PERIOD_MS);
+	// DF_playFolder(2, 1);
+	// while(true)
+	// {
+	// 	vTaskDelay(5000 / portTICK_PERIOD_MS);
+	// 	DF_playFolder(2, 1);
+	// }
 }
