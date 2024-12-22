fca434956cf53e109d7338fc5b5838a5fdbcf843
Pedro Centenaro <phc7blu@gmail.com>
2024-12-22 20:04:47 -0300

    Updated to test latest LCD funcionalities

--- main/main.c
+++ main/main.c
@@ -1,8 +1,20 @@
+#include "rom/ets_sys.h"
 #include "freertos/FreeRTOS.h"
-#include "PCF8574.h"
+#include "lcd.h"
 #include "sdkconfig.h"
 
 void app_main(void)
 {
-    PCF8574_init(CONFIG_PCF8574_SDA_PIN, CONFIG_PCF8574_SCL_PIN, CONFIG_PCF8574_SCL_SPEED_HZ, CONFIG_PCF8574_DEV_ADDR);
+    lcd_init();
+    while(true)
+    {
+        lcd_write("This is a test", 0);
+        lcd_write_center("Center!", 1);
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
+        lcd_write("This is a test", 1);
+        lcd_write_center("Center!", 0);
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
+    }
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    lcd_end();
 }
